021aee2f14aed79fd2d809384cf1ccb5805d18a5
Alexander <a.thorik@yandex.ru>
2024-09-20 22:42:52 +0300

    client init

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-8xl font-bold text-slate-800">Hello World!</h1>
+    </main>
+  );
+}
+
+export default App;
