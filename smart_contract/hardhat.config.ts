c7bf9e8f047dc42d445a5159d637beda0a479737
Alexander <a.thorik@yandex.ru>
2024-09-21 12:01:15 +0300

    update: обновлен конфиг, добавлена привязка к метамаск и alchemy

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    mainnet: {
+      url: process.env.ALCHEMY_LINK,
+      accounts: [
+        process.env.METAMASK_KEY !== undefined ? process.env.METAMASK_KEY : "",
+      ],
+    },
+  },
 };
 
 export default config;
